3b4bbf7ed21fa2d461731e441df633352749b02a
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-07-25 21:46:13 +0000

    learning javascript

--- javascript.js
+++ javascript.js
@@ -1,1 +1,73 @@
 
+// variables // 
+
+// let myAge = 30;   
+
+// let year = 2024 
+
+
+// console.log(myAge, year);
+
+
+
+// constants //
+
+// const myAge = 50;
+
+// console.log(myAge)
+
+// string concatenation
+
+// const firstName = "Emmanuel"
+// const lastName = "Tetteh"
+// const myAge = 30;
+
+// const fullName = firstName + " " + lastName + " " + myAge;
+
+// console.log(fullName);
+
+
+// console.log(fullName.length);
+
+
+// console.log(lastName[3]);
+
+
+// // common strings methods 
+
+// email = "emmanueltdortey@yahoo.com";
+
+// result = email.lastIndexOf('t');
+// console.log(result);
+
+// result = email.slice(6,15);
+// console.log(result);
+
+// // numbers 
+
+// let tep = 30;
+// const tepi = 50;
+
+// + - * / ** //-- exponentiating  // 
+// %
+
+// let radius = 10;
+// const pi = 3.14;
+
+// const area = pi * radius * radius;
+// console.log(area);
+
+// Template Strings 
+
+const firstName = "Emmanuel"
+const lastName = "Tetteh"
+const myAge = 30;
+
+fullName = `My name is ${firstName + " " + lastName} i am ${myAge} years old`;
+
+// console.log(`My name is `, firstName + lastName, `i am`, myAge);
+
+console.log(fullName);
+
+
+
