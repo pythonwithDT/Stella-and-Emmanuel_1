400e59a540f499b1701754165837bbc98fe3098a
Emmanuel Tetteh <emmanueltdortey@yahoo.com>
2024-08-08 22:01:34 +0000

    DOM

---
+++ New Branch/booleans.js
@@ -0,0 +1,51 @@
+// //  0 and 23
+
+// // ex 1
+
+//  const hour = 13;
+//  const name = 'Emmanuel'
+
+//  if ( hour >= 6 && hour <= 12 ) {
+//     console.log(`Good Morning ${name}`)
+//  } else if ( hour >= 13 && hour <= 17) {
+//     console.log(`Good Afternoon ${name}`)
+//  } else {
+//     console.log(`Good Night ${name}`)
+//  };
+
+
+// // ex 2
+
+//  const age = 27;
+//  const holiday = true;
+
+//  if (age <= 6 || age >= 65 && !holiday) {
+//     console.log('Discount');
+//  } else {
+//     console.log('No discount');
+//  };
+
+
+const randomNum = Math.random();
+
+let result = ''
+
+const guess = 'tails'
+
+if ( randomNum <= 0.5) {
+    result = 'heads'
+} else {
+    result = 'tails'
+}
+
+if ( guess === result) {
+    console.log('You win')
+} else {
+    console.log('You lose')
+}
+
+
+console.log(result);
+
+
+
